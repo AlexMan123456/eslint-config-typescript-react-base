1ee3445e2f17ad07613b7094c66c057a9547045f
AlexMan123456 <alex.man03888@gmail.com>
2025-07-13 03:52:14 +0100

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import alexBaseConfig from "@alextheman/eslint-config-typescript-base";
+
+export default [
+  js.configs.recommended,
+  ...alexBaseConfig,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: importPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+];
